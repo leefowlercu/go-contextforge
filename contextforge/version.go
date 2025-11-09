10b6983a4ba4abfcc629e835615fd5efa0ca01c3
Lee Fowler <leefowler.cu@gmail.com>
2025-11-09 02:36:48 -0500

    release: prepare v0.2.0

--- contextforge/version.go
+++ contextforge/version.go
@@ -2,4 +2,4 @@ package contextforge
 
 // Version is the current release version of the go-contextforge SDK.
 // This version is included in the User-Agent header for all API requests.
-const Version = "0.1.0"
+const Version = "0.2.0"
